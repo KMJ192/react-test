2487532f532abcb20105fb1e0872a66f4a3c70b8
KMJ192 <myeongjun222@naver.com>
2021-05-27 14:08:57 +0900

    test

--- src/components/coin_api/CoinAPI.tsx
+++ src/components/coin_api/CoinAPI.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { debounce } from 'lodash';
+import { debounce, fromPairs } from 'lodash';
 import { Bar } from 'react-chartjs-2';
+import { Chart } from 'chart.js';
 
 const bitsum_url = "https://api.bithumb.com/public/ticker/{order_currency}_{payment_currency}";
 let reqURL_krw : string = bitsum_url;
@@ -22,21 +23,6 @@ function CoinAPI() {
         units_traded: "",
         units_traded_24H: ""
     });
-
-    const data = {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [
-          {
-            label: 'test',
-            backgroundColor: 'rgba(255,99,132,0.2)',
-            borderColor: 'rgba(255,99,132,1)',
-            borderWidth: 1,
-            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-            hoverBorderColor: 'rgba(255,99,132,1)',
-            data: [65, 59, 80, 81, 56, 55, 40]
-          }
-        ]
-      };
 
     useEffect(() => {
         debounce(async () => {
@@ -59,6 +45,28 @@ function CoinAPI() {
         }, 1500)();
     }, [allData]);
 
+    const label = ['시가', '종가', '변동가(24H)', '변동률(24H)', '최고가', '최저가'];
+    const data = {
+        labels: label,
+        datasets: [
+          {
+            label: 'BTC',
+            backgroundColor: 'rgba(255,99,132,0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+            borderWidth: 1,
+            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+            hoverBorderColor: 'rgba(255,99,132,1)',
+            data: [
+                    allData.opening_price, 
+                    allData.closing_price, 
+                    allData.fluctate_24H,
+                    allData.fluctate_rate_24H, 
+                    allData.max_price, 
+                    allData.min_price
+                ]
+          }
+        ]
+    };
 
     return (
         <div>
@@ -80,13 +88,15 @@ function CoinAPI() {
             <br/>
             <br/>
             <div>
+                {/* type :  'line' | 'bar' | 'horizontalBar' | 'radar' | 'doughnut' | 'polarArea' | 'bubble' | 'pie' | 'scatter' */}
                 <Bar
-                    type="bal"
+                    type='horizontalBar'
                     data={data}
                     width={100}
-                    height={50}
+                    height={500}
                     options={{
-                        maintainAspectRatio: false
+                        maintainAspectRatio: false,
+                        
                     }}
                 />
             </div>
