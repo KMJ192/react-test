61c4db1dbd0e57d7ff0fa324a38fe9776db9b93e
KMJ192 <myeongjun222@naver.com>
2021-05-31 14:19:44 +0900

    test

--- src/components/coin_api/CoinAPI.tsx
+++ src/components/coin_api/CoinAPI.tsx
@@ -8,8 +8,22 @@ let reqURL_krw : string = bitsum_url;
 reqURL_krw = reqURL_krw.replace("{order_currency}", "all");
 reqURL_krw = reqURL_krw.replace("{payment_currency}", "KRW");
 
+interface ApiType {
+    acc_trade_value: string,
+    acc_trade_value_24H: string,
+    closing_price: string,
+    fluctate_24H: string,
+    fluctate_rate_24H: string,
+    max_price: string,
+    min_price: string,
+    opening_price: string,
+    prev_closing_price: string,
+    units_traded: string,
+    units_traded_24H: string
+}
+
 function CoinAPI() {
-    const [allData, setAllData] = useState({
+    const [allData, setAllData] = useState<ApiType>({
         acc_trade_value: "",
         acc_trade_value_24H: "",
         closing_price: "",
@@ -25,21 +39,21 @@ function CoinAPI() {
 
     useEffect(() => {
         debounce(async () => {
-            const response = await axios.get(reqURL_krw)
+            const response : ApiType = await axios.get(reqURL_krw)
                 .then(response => response.data.data.BTC)
                 .catch(err => err);
             setAllData({
-                acc_trade_value: response["acc_trade_value"],
-                acc_trade_value_24H: response["acc_trade_value_24H"],
-                closing_price: response["closing_price"],
-                fluctate_24H: response["fluctate_24H"],
-                fluctate_rate_24H: response["fluctate_rate_24H"],
-                max_price: response["max_price"],
-                min_price: response["min_price"],
-                opening_price: response["opening_price"],
-                prev_closing_price: response["prev_closing_price"],
-                units_traded: response["units_traded"],
-                units_traded_24H: response["units_traded_24H"]
+                acc_trade_value: response.acc_trade_value,
+                acc_trade_value_24H: response.acc_trade_value_24H,
+                closing_price: response.closing_price,
+                fluctate_24H: response.fluctate_24H,
+                fluctate_rate_24H: response.fluctate_rate_24H,
+                max_price: response.max_price,
+                min_price: response.min_price,
+                opening_price: response.opening_price,
+                prev_closing_price: response.prev_closing_price,
+                units_traded: response.units_traded,
+                units_traded_24H: response.units_traded_24H
             });
         }, 1500)();
     }, [allData]);
