af277691ffc5840ad34b3b456717e550170915cc
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2021-07-08 22:42:47 +0900

    test

--- src/components/chart_test/Chart.tsx
+++ src/components/chart_test/Chart.tsx
@@ -1,25 +1,85 @@
-import React from 'react'
-import { createChart } from 'lightweight-charts'
+import React, { useEffect } from 'react'
 import { useRef } from 'react'
+import { createChart, CrosshairMode, IChartApi } from 'lightweight-charts'
+
+import { ChartSt } from './MainChartStyle';
+import { priceData } from './PriceData';
+import { volumeData } from './VolumData';
 
 function Chart() {
-    const chartSample = useRef<any>(null);
-    const makeChart = () => {
-        let chart = createChart(chartSample.current, {
-            width: 500,
-            height: 300,
+    const chartContainerRef = useRef<any>(null);
+    const chart = useRef<IChartApi>();
+    //const resizeObserver = useRef<any>();
+
+    useEffect(() => {
+        chart.current = createChart(chartContainerRef.current, {
+            width: 1080,
+            height: 760,
             layout: {
-                backgroundColor: "#000000",
-                textColor: "rgba(255, 255, 255, 0)"
+                backgroundColor: "rgba(0, 0, 0, .9)",
+                textColor: "rgba(255, 255, 255, .6)"
             },
+            grid: {
+                vertLines:{
+                    color: 'rgba(197, 203, 206, 0.5)',
+                },
+                horzLines:{
+                    color: 'rgba(197, 203, 206, 0.5)',
+                }
+            },
+            crosshair:{
+                mode: CrosshairMode.Magnet
+            },
+            rightPriceScale: {
+                borderColor: 'rgba(197, 203, 206, 0.8)',
+            },
+            timeScale:{
+                borderColor: 'rgba(197, 203, 206, 0.8)',
+            }
         });
-    }
+        const color = {
+            upColor: "rgba(255, 0, 0, .7)",
+            downColor: "rgba(0,0,255,.5)"
+        }
+        const candleSeries = chart.current.addCandlestickSeries({
+            upColor: color.upColor,
+            downColor: color.downColor,
+            borderUpColor: color.upColor,
+            borderDownColor: color.downColor,
+            wickUpColor: color.upColor,
+            wickDownColor: color.downColor,
+        });
+        //setData 옵션 => time, open, high, low, close
+        candleSeries.setData(priceData);
+
+        // const volumeSeries = chart.current.addHistogramSeries({
+        //     color: '#182233',
+        //     lineWidth: 2,
+        //     priceFormat: {
+        //         type: 'volume',
+        //     },
+        //     overlay: true,
+        //     lastValueVisible: true,
+        //     scaleMargins: {
+        //         top: 0.8,
+        //         bottom: 0,
+        //     },
+        // });
+        // volumeSeries.setData(volumeData);
+    }, []);
+
+    // useEffect(() => {
+    //     resizeObserver.current = new ResizeObserver
+    //     return () => {
+            
+    //     }
+    // }, []);
+    
 
     return (
-        <div ref={chartSample}>
-            chart 출력
-        </div>
-    )
+        <ChartSt.Container ref={chartContainerRef}/>
+
+    );
 }
 
 export default React.memo(Chart);
---
+++ src/components/chart_test/MainChartStyle.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const ChartSt = {
+    Container: styled.div`
+        display: grid;
+        justify-content: center;
+        margin-top: 50px;
+    `
+}
